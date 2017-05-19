d2119b7e3df3108d80f13870d0e1ea22b488c846
Jack McDade <jack@jackmcdade.com>
2017-05-19 19:32:26 -0400

    better homepage content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
         <div class="content">
             <h1>RSS</h1>
             <h2>For the Rest of Us</h2>
-            <p>RSS is not dead! There's a <a href="https://jsonfeed.org/version/1" target="_blank">new format</a> in town, replacing XML with JSON, making developers happy everywhere. RSS is more alive than ever.
+            <p>This is the RSS Reader built for the rest of us. Simple and easy to use, <strong>it's like Netflix for your feeds</strong> and uses the new <a href="https://jsonfeed.org/version/1" target="_blank">new JSON format</a>. XML is dead. Long live RSS.</p>
         </div>
     </div>
 @stop

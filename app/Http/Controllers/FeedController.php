9f86304766f1b3f19b4a08c80ddd2e7f948f98d0
Jason Varga <jason@pixelfear.com>
2017-05-19 17:54:04 -0400

    put feeds behind auth middleware

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -8,6 +8,11 @@ use App\Exceptions\InvalidFeedException;
 
 class FeedController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *

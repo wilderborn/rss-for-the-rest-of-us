28196b60bf73ea5d41b20186bf7a2ef6a3cced1d
Jack McDade <jack@jackmcdade.com>
2017-05-19 16:23:14 -0400

    logged in/out nav states

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -5,8 +5,13 @@
     </a>
 
     <ul class="links">
-        <li><a href="{{ route('feeds.create') }}" class="active">Add Feed</a></li>
-        <li><a href="{{ route('feeds.index') }}">Your Feeds</a></li>
-        <li><a href="/logout">Logout</a></li>
+        @if (Auth::check())
+            <li><a href="{{ route('feeds.create') }}" class="active">Add Feed</a></li>
+            <li><a href="{{ route('feeds.index') }}">Your Feeds</a></li>
+            <li><a href="/logout">Logout</a></li>
+        @else
+            <li><a href="/register">Register</a></li>
+            <li><a href="/login">Login</a></li>
+        @endif
     </ul>
 </nav>

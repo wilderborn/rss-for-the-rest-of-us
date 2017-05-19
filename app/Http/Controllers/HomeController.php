60c9126e694ecfc85cf8a538eccbd1a8d29833ef
Jason Varga <jason@pixelfear.com>
2017-05-19 16:24:36 -0400

    Redirect homepage to feeds if logged in

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Middleware\RedirectToFeeds;
 
 class HomeController extends Controller
 {
@@ -13,7 +14,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware(RedirectToFeeds::class);
     }
 
     /**

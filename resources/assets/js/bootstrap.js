9e67d1be0f88a18fbdfa62a760a70f9b5c6301dc
Jack McDade <jack@jackmcdade.com>
2017-05-19 15:55:55 -0400

    rip out bootstrap

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,17 +1,4 @@
-
 window._ = require('lodash');
-
-/**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
- */
-
-try {
-    window.$ = window.jQuery = require('jquery');
-
-    require('bootstrap-sass');
-} catch (e) {}
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests

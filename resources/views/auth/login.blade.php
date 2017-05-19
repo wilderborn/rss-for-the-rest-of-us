60acc8281121fd9c85f46986d3b62c984440886d
Jack McDade <jack@jackmcdade.com>
2017-05-19 16:35:13 -0400

    page titles and google analytics

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
+@section('title', "Login")
 
 @section('content')
 <div class="container">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
+@section('title', "Create your account")
 
 @section('content')
 <div class="container">

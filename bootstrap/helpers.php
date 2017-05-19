233b90a182d05eebe0aa89560265a678fa2112f6
Jack McDade <jack@jackmcdade.com>
2017-05-19 15:55:43 -0400

    add gravatar and svg helpers

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,1 +1,17 @@
 <?php
+
+function gravatar($email)
+{
+    return "https://www.gravatar.com/avatar/" . e(md5(strtolower($email)));
+}
+
+/**
+ * SVG helper
+ *
+ * @param string $src Path to svg in the image directory
+ * @return string
+ */
+function svg($src)
+{
+    return file_get_contents(public_path('img/' . $src . '.svg'));
+}

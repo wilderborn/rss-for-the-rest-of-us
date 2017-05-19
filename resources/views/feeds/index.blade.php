cf08181258f328ac663209419e85841731c5a7d3
Jason Varga <jason@pixelfear.com>
2017-05-19 17:39:28 -0400

    Add icon as background to feed listing

--- resources/views/feeds/index.blade.php
+++ resources/views/feeds/index.blade.php
@@ -11,13 +11,20 @@
         </div>
         @foreach ($feeds as $feed)
             <div class="item">
-                <a href="{{ route('feeds.show', $feed) }}" class="feed-cover">
-                    <h2 class="feed-title">{{ $feed->title }}</h2>
+                <a href="{{ route('feeds.show', $feed) }}" class="feed-cover" style="background-image: url('{{ $feed->icon }}');">
+                    @if (! $feed->icon)
+                        <h2 class="feed-title">{{ $feed->title }}</h2>
+                    @endif
+
                     <div class="feed-count"><span>{{ $feed->count }}</span></div>
                 </a>
                 <div class="feed-description">
-                    {{ $feed->description }}
-                    {{-- {{ $feed->url }} --}}
+                    @if ($feed->icon)
+                        <h3>{{ $feed->title }}</h3>
+                    @endif
+                    @if ($feed->description)
+                        <p>{{ $feed->description }}</p>
+                    @endif
                 </div>
             </div>
         @endforeach

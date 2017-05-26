736341e422931b2de7722a780b72e8431ec410f2
Jack McDade <jack@jackmcdade.com>
2017-05-26 16:54:05 -0400

    Better feed index view

--- resources/views/feeds/show.blade.php
+++ resources/views/feeds/show.blade.php
@@ -3,7 +3,13 @@
 
 @section('content')
     <header class="page-header flexy">
-        <h1 class="fill">{{ $feed->title }}</h1>
+        <div class="fill" v-pre>
+            <h1>
+                {{ $feed->title }}
+                <small class="badge">({{ $feed->count }})</small>
+            </h1>
+            <p>{{ $feed->description }}</p>
+        </div>
         <form action="{{ route('feeds.destroy', $feed) }}" method="POST">
             {{ csrf_field() }}
             {{ method_field('DELETE') }}
@@ -11,29 +17,22 @@
         </form>
     </header>
     
-    <section class="main">    
+    <section class="main">
         <div class="read">
-            <div class="content" v-pre>
-                <h2>{{ $feed->title }} <span class="badge">{{ $feed->count }}</span></h2>
-                <p>{{ $feed->description }}</p>
-
-                @if ($feed->icon)
-                    <img src="{{ $feed->icon }}" />
-                @endif
-
-                @foreach ($feed->items as $item)
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <a href="{{ route('feeds.items.show', [$feed, 'id' => $item->id]) }}">#</a>
-                            <a href="{{ $item->url }}">{{ $item->title }}</a>
-                        </div>
-                        <div class="panel-body" v-pre>
-                            {!! $item->content_html !!}
-                        </div>
+            @foreach ($feed->items as $item)
+                <div class="feed-item">
+                    @if ($item->image)
+                    <div class="media">
+                        <img src="{{ $item->image }}">
                     </div>
-                @endforeach
-            </div>
-        </div>
-        
+                    @endif
+                    <div class="content">
+                        <h2><a href="{{ route('feeds.items.show', [$feed, 'id' => $item->id]) }}">{{ $item->title }}</a></h2>
+                        <p><em>{{ \Carbon\Carbon::parse($item->date_published)->diffForHumans() }}</em></p>
+                        {!! $item->summary !!}
+                    </div>
+                </div>
+            @endforeach
+        </div>        
     </section>
 @endsection
